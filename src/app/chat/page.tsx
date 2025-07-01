945723c2d4b0f02ab00cb6c15aff4f04ae63fe61
sylesh7 <sylesh.27it@licet.ac.in>
2025-07-01 20:32:40 +0530

    Feat : Included navbar in chat page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 import ChatBox from "./ChatBox";
+import Navbar from "../components/Navbar";
 
 export default function ChatPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <Navbar />
       <ChatBox />
     </div>
   );
